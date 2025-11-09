ede4d159578dbf6b2e55127a78c15d6d8f99fdfd
Maurice Yang <mauriceyangmy28@gmail.com>
2025-11-10 01:13:30 +0700

    fix: removing some redundant chars

--- src/FML/Transpiler.hs
+++ src/FML/Transpiler.hs
@@ -2,27 +2,26 @@
 
 module FML.Transpiler where
 
-import FML.Grammar
-  ( Attribute (..),
-    FML (FMLComponent, FMLScript),
-    FMLElement (..),
-    FMLExpr (..),
-    Prop (..),
-    ScriptExpr (..),
-  )
+import FML.Grammar (
+        Attribute (..),
+        FML (FMLComponent, FMLScript),
+        FMLElement (..),
+        FMLExpr (..),
+        Prop (..),
+        ScriptExpr (..),
+ )
 
 transpile :: [FML] -> String
 transpile components = unlines $ map transpileComponent components
 
 transpileComponent :: FML -> String
 transpileComponent (FMLComponent name props body) =
-  "export default function " ++ name ++ "(" ++ transpileProps props ++ ") {\n  return " ++ transpileElement body ++ ";\n}"
-transpileComponent (FMLScript scriptExprs) =
-  "// Script content\n" ++ unlines (map transpileScriptExpr scriptExprs)
+        "function " ++ name ++ "(" ++ transpileProps props ++ ") {\n  return " ++ transpileElement body ++ ";\n}"
+transpileComponent (FMLScript scriptExprs) = unlines (map transpileScriptExpr scriptExprs)
 
 transpileScriptExpr :: ScriptExpr -> String
 transpileScriptExpr (FMLDeclaration (SignalDeclaration name typeStr value)) =
-  "let [" ++ name ++ ", set" ++ name ++ "] = " ++ "createSignal(" ++ value ++ ");"
+        "let [" ++ name ++ ", set" ++ name ++ "] = " ++ "createSignal(" ++ value ++ ");"
 transpileScriptExpr (JSExpr s) = s
 transpileScriptExpr _ = ""
 
@@ -35,28 +34,28 @@ transpileProp (Prop name _) = name
 
 transpileElement :: FMLElement -> String
 transpileElement (FMLLiteral txt) =
-  show txt
+        show txt
 transpileElement (FMLExpression expr) =
-  expr
+        expr
 transpileElement (FMLElement tag attrs children) =
-  "f(" ++ show tag ++ "," ++ transpileAttributes attrs ++ transpileChildren children ++ ")"
+        "f(" ++ show tag ++ "," ++ transpileAttributes attrs ++ transpileChildren children ++ ")"
 
 transpileAttributes :: [Attribute] -> String
 transpileAttributes [] = "{}"
 transpileAttributes attrs =
-  "{" ++ joinWithComma (map transpileAttr attrs) ++ "}"
+        "{" ++ joinWithComma (map transpileAttr attrs) ++ "}"
 
 transpileAttr :: Attribute -> String
 transpileAttr (Attribute key val)
-  | key == "class" = showKeyVal "className" val
-  | otherwise = showKeyVal key val
-  where
-    showKeyVal k v = show k ++ ": " ++ show v
+        | key == "class" = showKeyVal "className" val
+        | otherwise = showKeyVal key val
+    where
+        showKeyVal k v = show k ++ ": " ++ show v
 
 transpileChildren :: [FMLElement] -> String
 transpileChildren [] = ""
 transpileChildren xs =
-  "," ++ joinWithComma (map transpileElement xs)
+        "," ++ joinWithComma (map transpileElement xs)
 
 joinWithComma :: [String] -> String
 joinWithComma [] = ""
